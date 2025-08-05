396f15c88fabc519579f3dc9be88d34e0f2e5276
tdang-healthcaresuccess <tony@healthcaresuccess.com>
2025-08-04 17:40:55 -0700

    Updated find care page text for testing

--- wp-templates/find-page.js
+++ wp-templates/find-page.js
@@ -1,99 +1,33 @@
-import { gql, useQuery } from "@apollo/client";
-import Head from "next/head";
-import EntryHeader from "../components/EntryHeader";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
-import { SITE_DATA_QUERY } from "../queries/SiteSettingsQuery";
-import { HEADER_MENU_QUERY } from "../queries/MenuQueries";
-import { getNextStaticProps } from "@faustwp/core";
+import { gql } from "@apollo/client";
 
-const PAGE_QUERY = gql`
-  query GetPage($databaseId: ID!, $asPreview: Boolean = false) {
-    page(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
-      title
-      content
-    }
-  }
-`;
-
-export default function SinglePage(props) {
-  if (props.loading) {
-    return <>Loading...</>;
-  }
-
-  const databaseId = props.__SEED_NODE__.databaseId;
-  const asPreview = props.__SEED_NODE__.asPreview;
-
-  const {
-    data,
-    loading = true,
-    error,
-  } = useQuery(PAGE_QUERY, {
-    variables: {
-      databaseId: databaseId,
-      asPreview: asPreview,
-    },
-    notifyOnNetworkStatusChange: true,
-    fetchPolicy: "cache-and-network",
-  });
-
-  const siteDataQuery = useQuery(SITE_DATA_QUERY) || {};
-  const headerMenuDataQuery = useQuery(HEADER_MENU_QUERY) || {};
-
-  if (loading && !data)
-    return (
-      <div className="container-main flex justify-center py-20">Loading...</div>
-    );
-
-  if (error) return <p>Error! {error.message}</p>;
-
-  if (!data?.page) {
-    return <p>No pages have been published</p>;
-  }
-
-
-  const siteData = siteDataQuery?.data?.generalSettings || {};
-  const menuItems = headerMenuDataQuery?.data?.primaryMenuItems?.nodes || {
-    nodes: [],
-  };
-  const { title: siteTitle, description: siteDescription } = siteData;
-  const { title, content } = data?.page || {};
-
+// The Component is required
+export default function Component(props) {
   return (
     <>
-      <Head>
-        <title>{`${title} - ${siteTitle}`}</title>
-      </Head>
-
-      <Header
-        siteTitle={siteTitle}
-        siteDescription={siteDescription}
-        menuItems={menuItems}
-      />
-
-      <main className="container">
-        <EntryHeader title={title} />
-        <div dangerouslySetInnerHTML={{ __html: content }} />
-      </main>
-
-      <Footer />
+    <div>Help</div>
     </>
   );
 }
 
+Component.query = gql`
+  query GetPageDataByURI($uri: ID!) {
+    page(id: $uri, idType: URI) {
+      title
+      content
+      slug
+    }
+  }
+`;
 
-SinglePage.queries = [
-  {
-    query: PAGE_QUERY,
-    variables: ({ databaseId }, ctx) => ({
-      databaseId,
-      asPreview: ctx?.asPreview,
-    }),
-  },
-  {
-    query: SITE_DATA_QUERY,
-  },
-  {
-    query: HEADER_MENU_QUERY,
-  },
-];
+Component.variables = (seedQuery, context) => {
+  return {
+    uri: seedQuery?.uri,
+  };
+};
+
+
+Component.variables = (seedQuery, context) => {
+  return {
+    uri: seedQuery?.uri,
+  };
+};
