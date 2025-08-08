513d998ddcf4f026de73aa786bd645eadd5f2b8a
tdang-healthcaresuccess <tony@healthcaresuccess.com>
2025-08-07 21:15:22 -0700

    Removed findcare-page template

--- wp-templates/index.js
+++ wp-templates/index.js
@@ -2,12 +2,10 @@ import frontPage from "./front-page";
 import page from "./page";
 import single from "./single";
 import archive from "./archive";
-import findCare from "./findcare-page";
 
 export default {
   "front-page": frontPage,
   page,
   single,
-  "page-findcare-page": findCare,
   archive,
 };
