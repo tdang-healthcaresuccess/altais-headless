5cbfa63387307e9fdfcf7cef64887ec8bef9e55e
xpertwebapp <appweb.live@gmail.com>
2025-08-11 13:26:05 +0530

    fixes

--- pages/template-a.js
+++ pages/template-a.js
@@ -1,10 +1,27 @@
 import Section1a from "@/components/acf/Section1a";
+import Section2a from "@/components/acf/Section2a";
+import Section3a from "@/components/acf/Section3a";
+import Section4a from "@/components/acf/Section4a";
+import Section5a from "@/components/acf/Section5a";
 import Breadcrumb from "@/components/common/breadcrumb";
 import InnerPageBanner from "@/components/common/inner-page-banner";
 import Layout from "@/components/Layout";
-import TemplateAContent from "@/components/template-content/template-a-content";
+import { PAGE_QUERY } from "@/wp-templates/pageQuery";
+import { useQuery } from "@apollo/client";
 
 export default function TemplateA() {
+  const { loading, error, data } = useQuery(PAGE_QUERY, {
+    variables: { databaseId: "/template-a", idType: "URI", asPreview: false },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const page = data?.page;
+  const templateSelection = page?.contentTemplates?.templateSelection;
+  const templateAContent = page?.contentTemplates?.templateA;
+  console.log(templateSelection);
+  
   return (
     <Layout>
       <div className="block">
@@ -23,7 +40,29 @@ export default function TemplateA() {
       {/* Breadcrumb End */}
 
       {/* Template A Start */}
-      <Section1a />
+      {templateSelection === "Template 1A" && templateAContent && (
+        <div className="acf-flexible-content">
+          {/* Iterate through templateA layouts */}
+          {templateAContent.map((layout, index) => {
+            const fieldGroupName = layout.fieldGroupName;
+            // Render components based on fieldGroupName
+            switch (fieldGroupName) {
+              case "ContentTemplatesTemplateASection1aLayout":
+                return <Section1a key={index} data={layout} />;
+              case "ContentTemplatesTemplateASection2aLayout":
+                return <Section2a key={index} data={layout} />;
+              case "ContentTemplatesTemplateASection3aLayout":
+                return <Section3a key={index} data={layout} />;
+              case "ContentTemplatesTemplateASection4aLayout":
+                return <Section4a key={index} data={layout} />;
+              case "ContentTemplatesTemplateASection5aLayout":
+                return <Section5a key={index} data={layout} />;
+              default:
+                return null;
+            }
+          })}
+        </div>
+      )}
       {/* Template A End */}
     </Layout>
   );
---
+++ wp-templates/pageQuery.js
@@ -0,0 +1,64 @@
+// queries/pageQuery.js
+import { gql } from "@apollo/client";
+
+export const PAGE_QUERY = gql`
+  query GetPage($databaseId: ID!, $asPreview: Boolean = false) {
+    page(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
+      title
+      content
+      contentTemplates {
+        templateSelection
+        templateA {
+          ... on ContentTemplatesTemplateASection1aLayout {
+            fieldGroupName
+            section1aContent
+            section1aImg {
+              node {
+                sourceUrl
+                uri
+              }
+            }
+          }
+          ... on ContentTemplatesTemplateASection2aLayout {
+            content2a
+            headline2a
+            sectionBackgroundColor
+            fieldGroupName
+          }
+          ... on ContentTemplatesTemplateASection3aLayout {
+            fieldGroupName
+            section3aCards {
+              cardContent
+              cardHeadline
+              cardOptions
+              fieldGroupName
+              cardImage {
+                node {
+                  uri
+                }
+              }
+            }
+          }
+          ... on ContentTemplatesTemplateASection4aLayout {
+            ctaButtonText
+            ctaButtonTextCopy
+            enableCta
+            fieldGroupName
+            section4aAdditionalHeadline
+            section4aAdditionalHeadlineOption
+            section4aHeadline
+            section4aImage {
+              node {
+                uri
+              }
+            }
+          }
+          ... on ContentTemplatesTemplateASection5aLayout {
+            fieldGroupName
+            section5aContent
+          }
+        }
+      }
+    }
+  }
+`;
