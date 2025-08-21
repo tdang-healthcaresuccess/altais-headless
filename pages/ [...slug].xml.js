79ddb9f1cba5de5155ce0feb708129de22af90d5
tdang-healthcaresuccess <tony@healthcaresuccess.com>
2025-08-21 12:14:12 -0700

    Another sitemap_xml update

--- pages/ [...slug].xml.js
+++ pages/ [...slug].xml.js
@@ -3,9 +3,8 @@ import { getSitemapProps } from '@faustwp/core';
 export default function Sitemap() {}
 
 export function getServerSideProps(ctx) {
-  // Pass the full URL, including the query string, to the Faust utility.
   return getSitemapProps(ctx, {
     frontendUrl: process.env.NEXT_PUBLIC_SITE_URL,
-    // The utility will handle the sitemap logic based on the URL.
+    sitemapIndexPath: 'sitemap_index.xml',
   });
 }
