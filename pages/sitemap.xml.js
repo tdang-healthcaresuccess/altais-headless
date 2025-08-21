46bc98e50f53ab4cd728656a79f7a65e96b75107
tdang-healthcaresuccess <tony@healthcaresuccess.com>
2025-08-21 09:37:29 -0700

    Updated sitemap.js and robot for RankMath

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,11 +1,10 @@
-import { getSitemapProps } from "@faustwp/core";
+import { getSitemapProps } from '@faustwp/core';
 
 export default function Sitemap() {}
 
 export function getServerSideProps(ctx) {
   return getSitemapProps(ctx, {
     frontendUrl: process.env.NEXT_PUBLIC_SITE_URL,
-    sitemapIndexPath: 'sitemap.xml',
-    sitemapPathsToIgnore: ['/wp-sitemap-users-*'],
+    sitemapIndexPath: 'sitemap_index.xml', // Path for RankMath
   });
 }
