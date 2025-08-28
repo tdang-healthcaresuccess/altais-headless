534d9b675b854109fccb57ff450e043ef56b161c
tdang-healthcaresuccess <tony@healthcaresuccess.com>
2025-08-27 20:01:13 -0700

    Added redirects in next.config.js

--- next.config.js
+++ next.config.js
@@ -8,8 +8,9 @@ module.exports = withFaust({
     domains: ["faustexample.wpengine.com"],
   },
   trailingSlash: true,
-    async redirects() {
+  async redirects() {
     return [
+      // Existing redirect rule
       {
         source: '/:path([^/]+)',
         destination: '/:path/',
@@ -22,6 +23,277 @@ module.exports = withFaust({
             negate: true,
           },
         ],
+      },
+      // Redirects from redirection.csv
+      {
+        source: '/about/who-we-are/',
+        destination: '/about/',
+        permanent: true,
+      },
+      {
+        source: '/altais-care-alliance/',
+        destination: '/insurances-accepted/medicare/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/employment-opportunities/',
+        destination: 'https://altaishealthsolutions.wd108.myworkdayjobs.com/en-US/External',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/employment-opportunities/careers/',
+        destination: 'https://altaishealthsolutions.wd108.myworkdayjobs.com/en-US/External',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/contactenos/',
+        destination: '/contact-us/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/mantengase-en-contacto/',
+        destination: '/contact-us/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/medicos/clara-chae-md/',
+        destination: '/physicians/es/clara-chae-md/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/medicos/colin-christensen-d-o/',
+        destination: '/physicians/es/colin-christensen-do/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/medicos/david-choi-m-d/',
+        destination: '/physicians/es/david-choi-md/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/medicos/eric-choi/',
+        destination: '/physicians/es/eric-choi/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/medicos/howard-aubert/',
+        destination: '/physicians/es/howard-aubert/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/medicos/kathleen-keating/',
+        destination: '/physicians/es/kathleen-keating/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/medicos/leah-hopkins/',
+        destination: '/physicians/es/leah-hopkins/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/medicos/rodolfo-saenz-m-d-m-m-m-f-a-c-o-g/',
+        destination: '/physicians/es/rodolfo-saenz-m-d-m-m-m-f-a-c-o-g/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/medicos/sarah-chae-m-d/',
+        destination: '/physicians/es/sarah-chae-md/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/medicos/william-suh-m-d-f-s-c-a-i/',
+        destination: '/physicians/es/william-suh-m-d-f-s-c-a-i/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/es/seguros/',
+        destination: '/contact-us/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/physicians/clara-chae/',
+        destination: '/physicians/clara-chae/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/physicians/colin-christensen/',
+        destination: '/physicians/colin-christensen/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/physicians/eric-choi/',
+        destination: '/physicians/eric-choi/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/physicians/howard-aubert-md/',
+        destination: '/physicians/howard-aubert-md/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/physicians/kathleen-keating/',
+        destination: '/physicians/kathleen-keating/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/physicians/leah-hopkins/',
+        destination: '/physicians/leah-hopkins/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/physicians/rodolfo-saenz/',
+        destination: '/physicians/rodolfo-saenz/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/physicians/sarah-chae/',
+        destination: '/physicians/sarah-chae/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/physicians/sergio-villegas-m-d/',
+        destination: '/physicians/sergio-villegas-md/',
+        permanent: true,
+      },
+      {
+        source: '/altais-medical-group-riverside/stay-in-touch/',
+        destination: '/contact-us/',
+        permanent: true,
+      },
+      {
+        source: '/amg/embrace-freshness-exploring-the-vibrant-farmers-markets-of-riverside-county/',
+        destination: '/blog/embrace-freshness-exploring-the-vibrant-farmers-markets-of-riverside-county/',
+        permanent: true,
+      },
+      {
+        source: '/amg/riverside-blog/autumn-apple-adventures-health-benefits-and-pick-your-own-fun-in-riverside/',
+        destination: '/blog/autumn-apple-adventures-health-benefits-and-pick-your-own-fun-in-riverside/',
+        permanent: true,
+      },
+      {
+        source: '/amg/riverside-blog/beating-the-heat-10-ways-to-stay-cool-and-healthy-in-riversides-sizzling-summers/',
+        destination: '/blog/beating-the-heat-10-ways-to-stay-cool-and-healthy-in-riversides-sizzling-summers/',
+        permanent: true,
+      },
+      {
+        source: '/amg/riverside-blog/do-a-gut-check-is-it-ibs-or-something-else/',
+        destination: '/blog/do-a-gut-check-is-it-ibs-or-something-else/',
+        permanent: true,
+      },
+      {
+        source: '/amg/riverside-blog/get-in-shape-with-hiking-trails-in-riverside-california/',
+        destination: '/blog/get-in-shape-with-hiking-trails-in-riverside-california/',
+        permanent: true,
+      },
+      {
+        source: '/amg/riverside-blog/mindful-movement-exploring-riversides-meditation-gardens-and-labyrinths/',
+        destination: '/blog/mindful-movement-exploring-riversides-meditation-gardens-and-labyrinths/',
+        permanent: true,
+      },
+      {
+        source: '/amg/riverside-blog/riversides-top-outdoor-yoga-spots-for-boosting-your-health-and-well-being/',
+        destination: '/blog/riversides-top-outdoor-yoga-spots-for-boosting-your-health-and-well-being/',
+        permanent: true,
+      },
+      {
+        source: '/brand-assets/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/brown-toland/',
+        destination: '/about/brown-and-toland/',
+        permanent: true,
+      },
+      {
+        source: '/category/amg/',
+        destination: '/blog/',
+        permanent: true,
+      },
+      {
+        source: '/category/amg/riverside-blog/',
+        destination: '/blog/',
+        permanent: true,
+      },
+      {
+        source: '/category/blog/',
+        destination: '/blog/',
+        permanent: true,
+      },
+      {
+        source: '/cookie-policy/',
+        destination: '/cookies/',
+        permanent: true,
+      },
+      {
+        source: '/family-care-specialists-medical-group/',
+        destination: '/about/fcsmg-family-care-specialists-medical-group/',
+        permanent: true,
+      },
+      {
+        source: '/fcs-ipa/',
+        destination: '/about/fcsmg-family-care-specialists-medical-group/',
+        permanent: true,
+      },
+      {
+        source: '/for-physicians/altais-networks/',
+        destination: '/for-providers/altais-networks/',
+        permanent: true,
+      },
+      {
+        source: '/for-physicians/code-of-conduct-and-compliance-training/',
+        destination: '/code-of-conduct-and-compliance-training/',
+        permanent: true,
+      },
+      {
+        source: '/jeff-bailet/',
+        destination: '/physicians/',
+        permanent: true,
+      },
+      {
+        source: '/medicos/',
+        destination: '/for-providers/amg-locations/',
+        permanent: true,
+      },
+      {
+        source: '/patients/',
+        destination: '/about/',
+        permanent: true,
+      },
+      {
+        source: '/privacy/',
+        destination: '/website-privacy/',
+        permanent: true,
+      },
+      {
+        source: '/robert-van-tuyl/',
+        destination: '/physicians/',
+        permanent: true,
+      },
+      {
+        source: '/services/care-delivery-networks/',
+        destination: '/for-partners/',
+        permanent: true,
+      },
+      {
+        source: '/services/health-systems/',
+        destination: '/for-partners/',
+        permanent: true,
+      },
+      {
+        source: '/services/payers/',
+        destination: '/for-partners/',
+        permanent: true,
+      },
+      {
+        source: '/services/physician-practices/',
+        destination: '/services/',
+        permanent: true,
+      },
+      {
+        source: '/what-we-do/',
+        destination: '/about/',
+        permanent: true,
       },
     ];
   },
