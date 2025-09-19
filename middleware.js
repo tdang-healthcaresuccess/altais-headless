fcee046b9c8aec1c4ee612913fc6399facf6555f
tdang-healthcaresuccess <tony@healthcaresuccess.com>
2025-09-19 11:22:08 -0700

    Updated middleware to exclude certain files that are case sensitive

--- middleware.js
+++ middleware.js
@@ -1,7 +1,19 @@
 import { NextResponse } from 'next/server';
-
 export function middleware(request) {
   const url = request.nextUrl;
+  // Exclude _next, static, media, favicon, HubSpot, and API paths from lowercasing
+  if (
+    url.pathname.startsWith('/_next') ||
+    url.pathname.startsWith('/static') ||
+    url.pathname.startsWith('/media') ||
+    url.pathname.startsWith('/favicon') ||
+    url.pathname.startsWith('/api') ||
+    url.pathname.startsWith('/hubspot') ||
+    url.pathname.startsWith('/hsforms') ||
+    url.pathname.startsWith('/js.hsforms.net')
+  ) {
+    return NextResponse.next();
+  }
   const lowerPath = url.pathname.toLowerCase();
   if (url.pathname !== lowerPath) {
     url.pathname = lowerPath;
